afcc2f206c8a123738a1e4a3fa2b8ac94b3e6604
vipul sharma <vipulsharmamtr@gmail.com>
2023-04-30 18:04:44 +0530

    Add files via upload
    count the number of elements strictly greater than value of X.

---
+++ StrictlyGreaterCount.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class StrictlyGreater {
+    
+    static int StrictlyGreaterCount (int arr[] , int x){
+
+        int StrictlyGreaterOFx = 0;
+
+        for ( int i = 0 ; i < arr.length ; i++){
+
+            if ( arr[i] > x ){
+
+                StrictlyGreaterOFx++;
+            }
+        }
+
+        return StrictlyGreaterOFx;
+     }
+
+     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" enter the size of array ");
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println(" enter the " + n + " elements of array ");
+
+        for( int i = 0 ; i < arr.length ; i++){
+
+            arr[i] = sc.nextInt();
+
+            
+        }
+
+        System.out.println(" enter the value of X ");
+
+            int x = sc.nextInt();
+
+            System.out.println("STRICTLY GREATER OF X:" + " " + StrictlyGreaterCount(arr, n) );
+
+        }
+
+    }
